ec43bdb86f367c83b63ccfbf4137ed33f4a08c6c
Diana Pshennikova <di.pshennikova@gmail.com>
2026-03-05 12:54:53 +0300

    6

--- docs/index.js
+++ docs/index.js
@@ -22,9 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }, {
     href: './practices.html',
     text: 'Практики'
-  },
-  // исправил "Пракики" → "Практики"
-  {
+  }, {
     href: './articles.html',
     text: 'Статьи'
   }];
@@ -35,32 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }).join(''), "\n        </div>\n        <a class=\"A_pText A_MenuTelegram\" href=\"https://t.me/pleaxy_media\">telegram</a>\n      </nav>\n    ");
   }
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const MENU_ITEMS = [
-//     { href: '/inventory.html', text: 'Инвентарь' },
-//     { href: '/practice.html', text: 'Практики' }, // исправил "Пракики" → "Практики"
-//     { href: '/articles.html', text: 'Статьи' }
-//   ]
-
-//   const menuContainer = document.getElementById('menu')
-//   if (menuContainer) {
-//     menuContainer.innerHTML = `
-//       <nav class="O_Menu">
-//         <a href="/" class="A_logo">
-//           <img src="${logo}" alt="pleaxy" />
-//         </a>
-//         <div class="M_Menu">
-//           ${MENU_ITEMS.map(
-//             (item) =>
-//               `<a class="A_MenuBottom" href="${item.href}">${item.text}</a>`
-//           ).join('')}
-//         </div>
-//         <a class="A_pText" href="https://t.me/pleaxy_media">telegram</a>
-//       </nav>
-//     `
-//   }
-// })
 ;// ./src/javascript/footer.js
 
 document.addEventListener('DOMContentLoaded', function () {
--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -2,7 +2,7 @@ import logo from '../images/logo.svg'
 document.addEventListener('DOMContentLoaded', function () {
   const MENU_ITEMS = [
     { href: './inventory.html', text: 'Инвентарь' },
-    { href: './practices.html', text: 'Практики' }, // исправил "Пракики" → "Практики"
+    { href: './practices.html', text: 'Практики' },
     { href: './articles.html', text: 'Статьи' }
   ]
 
@@ -24,29 +24,3 @@ document.addEventListener('DOMContentLoaded', function () {
     `
   }
 })
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const MENU_ITEMS = [
-//     { href: '/inventory.html', text: 'Инвентарь' },
-//     { href: '/practice.html', text: 'Практики' }, // исправил "Пракики" → "Практики"
-//     { href: '/articles.html', text: 'Статьи' }
-//   ]
-
-//   const menuContainer = document.getElementById('menu')
-//   if (menuContainer) {
-//     menuContainer.innerHTML = `
-//       <nav class="O_Menu">
-//         <a href="/" class="A_logo">
-//           <img src="${logo}" alt="pleaxy" />
-//         </a>
-//         <div class="M_Menu">
-//           ${MENU_ITEMS.map(
-//             (item) =>
-//               `<a class="A_MenuBottom" href="${item.href}">${item.text}</a>`
-//           ).join('')}
-//         </div>
-//         <a class="A_pText" href="https://t.me/pleaxy_media">telegram</a>
-//       </nav>
-//     `
-//   }
-// })
