8ff8742b55c2a0a10b9da8ba216f2a488df53aa0
Diana Pshennikova <di.pshennikova@gmail.com>
2026-03-05 11:16:02 +0300

    5

--- docs/index.js
+++ docs/index.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }];
   var menuContainer = document.getElementById('menu');
   if (menuContainer) {
-    menuContainer.outerHTML = " \n      <nav class=\"O_Menu\">\n        <a href=\"/\" class=\"A_logo\">\n          <img src=\"".concat(logo_namespaceObject, "\" alt=\"pleaxy\" />\n        </a>\n        <div class=\"M_Menu\">\n          ").concat(MENU_ITEMS.map(function (item) {
+    menuContainer.outerHTML = " \n      <nav class=\"O_Menu\">\n        <a href=\"./\" class=\"A_logo\">\n          <img src=\"".concat(logo_namespaceObject, "\" alt=\"pleaxy\" />\n        </a>\n        <div class=\"M_Menu\">\n          ").concat(MENU_ITEMS.map(function (item) {
       return "<a class=\"A_MenuBottom\" href=\"".concat(item.href, "\">").concat(item.text, "</a>");
     }).join(''), "\n        </div>\n        <a class=\"A_pText A_MenuTelegram\" href=\"https://t.me/pleaxy_media\">telegram</a>\n      </nav>\n    ");
   }
