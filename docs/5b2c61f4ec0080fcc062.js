b1fb3989c707f47fb066da62c7f63d580db86bf1
Diana Pshennikova <di.pshennikova@gmail.com>
2026-04-10 00:35:08 +0300

    fixing adaptations

--- docs/5b2c61f4ec0080fcc062.js
+++ docs/5b2c61f4ec0080fcc062.js
@@ -51,31 +51,25 @@
 //   }
 // })
 
-import logo from '../images/logo.svg'
-
+import logo from '../images/logo.svg';
 document.addEventListener('DOMContentLoaded', function () {
   // Авто: если в /pages/, BASE_PATH = '../' | иначе './'
-  const path = window.location.pathname
-  const BASE_PATH = path.includes('/pages/') ? '../' : './'
-
-  const MENU_ITEMS = [
-    { href: BASE_PATH + 'sextoys.html', text: 'Секс-игрушки' },
-    { href: BASE_PATH + 'practices.html', text: 'Практики' },
-    { href: BASE_PATH + 'articles.html', text: 'Статьи' }
-  ]
-
-  const menuContainer = document.getElementById('menu')
+  var path = window.location.pathname;
+  var BASE_PATH = path.includes('/pages/') ? '../' : './';
+  var MENU_ITEMS = [{
+    href: BASE_PATH + 'sextoys.html',
+    text: 'Секс-игрушки'
+  }, {
+    href: BASE_PATH + 'practices.html',
+    text: 'Практики'
+  }, {
+    href: BASE_PATH + 'articles.html',
+    text: 'Статьи'
+  }];
+  var menuContainer = document.getElementById('menu');
   if (menuContainer) {
-    menuContainer.outerHTML = `
-      <nav class="O_Menu">
-        <a href="${BASE_PATH === '../' ? '../' : './'}" class="A_logo">
-          <img src="${logo}" alt="pleaxy" />
-        </a>
-        <div class="M_Menu">
-          ${MENU_ITEMS.map((item) => `<a class="A_MenuBottom" href="${item.href}">${item.text}</a>`).join('')}
-        </div>
-        <a class="A_pText A_MenuTelegram" href="https://t.me/pleaxy_media">telegram</a>
-      </nav>
-    `
+    menuContainer.outerHTML = "\n      <nav class=\"O_Menu\">\n        <a href=\"".concat(BASE_PATH === '../' ? '../' : './', "\" class=\"A_logo\">\n          <img src=\"").concat(logo, "\" alt=\"pleaxy\" />\n        </a>\n        <div class=\"M_Menu\">\n          ").concat(MENU_ITEMS.map(function (item) {
+      return "<a class=\"A_MenuBottom\" href=\"".concat(item.href, "\">").concat(item.text, "</a>");
+    }).join(''), "\n        </div>\n        <a class=\"A_pText A_MenuTelegram\" href=\"https://t.me/pleaxy_media\">telegram</a>\n      </nav>\n    ");
   }
-})
+});
--- docs/index.js
+++ docs/index.js
@@ -71,22 +71,50 @@ const logo_namespaceObject = __webpack_require__.p + "images/2e8475c169cc155b7d3
 //   }
 // })
 
+// import logo from '../images/logo.svg'
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const BASE_URL = window.location.origin + '/'
+//   const MENU_ITEMS = [
+//     { href: BASE_URL + 'sextoys.html', text: 'Секс-игрушки' },
+//     { href: BASE_URL + 'practices.html', text: 'Практики' },
+//     { href: BASE_URL + 'articles.html', text: 'Статьи' }
+//   ]
+
+//   const menuContainer = document.getElementById('menu')
+//   if (menuContainer) {
+//     menuContainer.outerHTML = `
+//       <nav class="O_Menu">
+//         <a href="/" class="A_logo">
+//           <img src="${logo}" alt="pleaxy" />  <!-- logo уже полный URL от import -->
+//         </a>
+//         <div class="M_Menu">
+//           ${MENU_ITEMS.map((item) => `<a class="A_MenuBottom" href="${item.href}">${item.text}</a>`).join('')}
+//         </div>
+//         <a class="A_pText A_MenuTelegram" href="https://t.me/pleaxy_media">telegram</a>
+//       </nav>
+//     `
+//   }
+// })
+
 
 document.addEventListener('DOMContentLoaded', function () {
-  var BASE_URL = window.location.origin + '/';
+  // Авто: если в /pages/, BASE_PATH = '../' | иначе './'
+  var path = window.location.pathname;
+  var BASE_PATH = path.includes('/pages/') ? '../' : './';
   var MENU_ITEMS = [{
-    href: BASE_URL + 'sextoys.html',
+    href: BASE_PATH + 'sextoys.html',
     text: 'Секс-игрушки'
   }, {
-    href: BASE_URL + 'practices.html',
+    href: BASE_PATH + 'practices.html',
     text: 'Практики'
   }, {
-    href: BASE_URL + 'articles.html',
+    href: BASE_PATH + 'articles.html',
     text: 'Статьи'
   }];
   var menuContainer = document.getElementById('menu');
   if (menuContainer) {
-    menuContainer.outerHTML = "\n      <nav class=\"O_Menu\">\n        <a href=\"/\" class=\"A_logo\">\n          <img src=\"".concat(logo_namespaceObject, "\" alt=\"pleaxy\" />  <!-- logo \u0443\u0436\u0435 \u043F\u043E\u043B\u043D\u044B\u0439 URL \u043E\u0442 import -->\n        </a>\n        <div class=\"M_Menu\">\n          ").concat(MENU_ITEMS.map(function (item) {
+    menuContainer.outerHTML = "\n      <nav class=\"O_Menu\">\n        <a href=\"".concat(BASE_PATH === '../' ? '../' : './', "\" class=\"A_logo\">\n          <img src=\"").concat(logo_namespaceObject, "\" alt=\"pleaxy\" />\n        </a>\n        <div class=\"M_Menu\">\n          ").concat(MENU_ITEMS.map(function (item) {
       return "<a class=\"A_MenuBottom\" href=\"".concat(item.href, "\">").concat(item.text, "</a>");
     }).join(''), "\n        </div>\n        <a class=\"A_pText A_MenuTelegram\" href=\"https://t.me/pleaxy_media\">telegram</a>\n      </nav>\n    ");
   }
